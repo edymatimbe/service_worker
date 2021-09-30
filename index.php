c592e03157fc3f612dfa8c7fb02ed0b4b46284ab
edymatimbe <edymatimbe@gmail.com>
2021-09-30 15:11:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" href="images/icons.png">
+    <meta name="apple-mobile-web-app-status-bar" content="#aa7700">
+    <meta name="theme-color" content="#aa7700"/>
+    <title>My App</title>
+    <link rel="manifest" href="manifest.json">
+
+</head>
+<body>
+    <h1>Aboust uS Green Rev</h1>
+    <p>This function</p>
+    <script src="js/script.js"></script>
+</body>
+</html>
